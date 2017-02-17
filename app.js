ef9b7fa2744b2651d471ca41415c5409cb5a71ea
FlipperCoin <itai10020@gmail.com>
2017-02-17 22:22:22 +0200

    fix

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ function handleTTS(guild, message) {
 	(bot.voiceConnections.get(guild.id) ? new Promise(resolve => resolve()) : channel.join()).then(connection => {
 		tts(guild, message);
 	}, error => {
-		console.log(`Error in joining the channel. Error: ${error}`);
+		console.log(`Error in joining the voice channel. Error: ${error}`);
+		console.log(error);
 	}).catch((e) => {
 		console.log(`Exception in tts. Exception: ${e}`);
+		console.log(e);
 	});
 }
 
