3e9106091802f00f09c12973abb2e76bd2bdc6eb
FlipperCoin <itai10020@gmail.com>
2017-02-17 02:18:21 +0200

    fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ bot.on('message', message => {
 });
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
-	console.log(oldMember,newMember);
+	console.log(oldMember.voiceChannel,newMember.voiceChannel);
 	if (oldMember.voiceChannel != undefined && newMember.voiceChannel) {
 		let channel = getTextChannel(newMember.guild);
 		if (channel) {
